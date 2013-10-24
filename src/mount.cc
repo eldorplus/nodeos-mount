a6818c8714665236234beb8a2a7e9ede01e1cccf
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-10-24 18:39:58 +0300

    Made this function whole lot better!

--- src/mount.cc
+++ src/mount.cc
@@ -9,13 +9,28 @@ v8::Handle<v8::Value> Mount(const v8::Arguments &args) {
     // TODO: make it raise a proper exception
     return v8::ThrowException(v8::String::New("`mount` needs at least 3 parameters"));
   }
-  
+  bool callback = false;
+  v8::Local<v8::Function> cb;
+  if(args.Length() == 4) {
+    callback = true;
+    cb = v8::Local<v8::Function>::Cast(args[3]);
+  }
+
+
   v8::String::Utf8Value device(args[0]->ToString());
   v8::String::Utf8Value path(args[1]->ToString());
   v8::String::Utf8Value type(args[2]->ToString());
 
+  bool mountAction = (mount(*device, *path, *type, 0, NULL) == 0) ? true : false;
   // TODO: expose flag values
-  return (mount(*device, *path, *type, 0, NULL) == 0) ? v8::True() : v8::False();
+  v8::Local<v8::Value> mounted = v8::Local<v8::Value>::New(v8::Boolean::New(mountAction));
+
+  if(callback) {
+    const unsigned argc = 1;
+    v8::Local<v8::Value> argv[argc] = {mounted};
+    cb->Call(v8::Context::GetCurrent()->Global(), argc, argv);
+  }
+  return mounted;
 }
 
 v8::Handle<v8::Value> Umount(const v8::Arguments &args) {
@@ -30,9 +45,9 @@ v8::Handle<v8::Value> Umount(const v8::Arguments &args) {
   return (umount(*path) == 0) ? v8::True() : v8::False();
 }
 
-void init (v8::Handle<v8::Object> target) {
-  NODE_SET_METHOD(target, "mount", Mount);
-  NODE_SET_METHOD(target, "umount", Umount);
+void init (v8::Handle<v8::Object> exports, v8::Handle<v8::Object> module) {
+  exports->Set(v8::String::NewSymbol("mount"), v8::FunctionTemplate::New(Mount)->GetFunction());
+  exports->Set(v8::String::NewSymbol("umount"), v8::FunctionTemplate::New(Umount)->GetFunction());
 }
 NODE_MODULE(mount, init)
 
