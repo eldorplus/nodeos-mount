a183e0e3bcd265713adb491641ad7ee97dcd58c1
Patrick Stapfer <ps@spp.at>
2014-04-28 14:41:48 +0200

    Changed unmount to umount; Added more documentation in README; Preparation to make this addon asynchronous

--- README.md
+++ README.md
@@ -4,14 +4,54 @@ Mount/unmount devices from node.js
 
 Really works on linux, may work on OS X, and will never work on windows.
 
-## Installation
+This version was created to apply some small corrections and offers a more verbose documentation.
+The most opinionated change was the function name change of `unmount` to `umount` since I 
+like consistency through the whole stack.
 
+##Installation
+This package is not featured in NPM, so you have to add this file in your package.json
+file:
+
+**Part of package.json:**
 ```
-npm install mnt
+{
+    "name" : "myproject",
+    "version" : "1.0",
+    ...
+    ...
+    "dependencies" : {
+        "mount" : "git+ssh://git@github.com:magicpat/node-mount.git"
+    }
+}
 ```
+## Syntax
+
+### mount
+```javascript
+mount(devFile, mntPath, fsType, options, data)
+```
+- `devFile` - {String} Device-File being mounted (located in /dev)
+- `mntPath` - {String} Directory to mount the device to 
+- `options` - {Array.<String>} Array containing String options (see reference list)
+
+*options*
+These (limited number of) options are equivalent to the mountflags parameter used by
+the C mount function (http://man7.org/linux/man-pages/man2/mount.2.html). 
+Following options are available: 
+- `bind` 
+- `readonly`
+- `remount`
+- `noexec`
+
+### umount*
+```javascript
+umount(target)
+```
+- `target` - {String} Mount-Path to umount (equivalent to `mntPath` of `mount`)
 
 ## Usage
 
+*Mount Tmpfs:*
 ```javascript
 var mount = require('mount');
 mount.mount('tmpfs', 'tmpDir', 'tmpfs', function(success) {
@@ -22,4 +62,17 @@ mount.mount('tmpfs', 'tmpDir', 'tmpfs', function(success) {
 });
 ```
 
-This module is forked from Maciej Małecki`s [repository](https://github.com/mmalecki/node-mount), but completely rewritten.
+*Mount DVD:*
+```javascript
+var mount = require('mount');
+mount.mount('/dev/sr0', 'myDir', 'iso9660', function(success) {
+	// Do something 
+	mount.unmount('myDir', function(success) {
+		// Do something 
+	});
+});
+```
+
+## Credits
+- Directly forked from Stackdot`s [repository](https://github.com/stackdot/node-mount)
+- Stackdot forked this from Maciej Małecki`s [repository](https://github.com/mmalecki/node-mount)
--- src/mount.cc
+++ src/mount.cc
@@ -5,102 +5,124 @@
 #include <string.h>
 #include <iostream>
 
-v8::Handle<v8::Value> Mount(const v8::Arguments &args) {
-  v8::HandleScope scope;
+using namespace v8;
 
-  if (args.Length() < 3) {
-    // TODO: make it raise a proper exception
-    return v8::ThrowException(v8::String::New("`mount` needs at least 3 parameters"));
-  }
+//        0         1       2       3       4
+//mount(devFile, mntPath, fsType, options, data) 
+Handle<Value> Mount(const Arguments &args) {
+    HandleScope scope;
+    
+    int argsLength = args.Length();
+    if (argsLength <= 2) {
+        if (argsLength == 0){
+            return ThrowException(String::New("Missing 'devFile'"));
+        }
 
-  bool callback = false;
-  v8::Local<v8::Function> cb;
-  v8::Handle<v8::Array> options;
-  v8::Local<v8::String> data;
+        if (argsLength == 1){
+            return ThrowException(String::New("Missing 'mntPath'"));
+        }
 
-  if(args.Length() == 4) {
-    callback = true;
-    cb = v8::Local<v8::Function>::Cast(args[3]);
-    options = v8::Array::New(0);
-  } else if(args.Length() == 5) {
-    callback = true;
-    cb = v8::Local<v8::Function>::Cast(args[4]);
-    if (args[3]->IsArray()) {
-      options = v8::Handle<v8::Array>::Cast(args[3]);
+        if(argsLength == 2){
+            return ThrowException(String::New("Missing 'fsType'"));
+        }
     }
-  } else if(args.Length() == 6) {
-    callback = true;
-    cb = v8::Local<v8::Function>::Cast(args[5]);
-    if (args[3]->IsArray()) {
-      options = v8::Handle<v8::Array>::Cast(args[3]);
+   
+    Local<Function> cb;
+    Handle<Array> options;
+    Local<String> data;
+
+    if(!args[0]->IsString() || !args[1]->IsString() || !args[2]->IsString()){
+        return ThrowException(String::New("Wrong argument types... expected strings"));
     }
-    if (args[4]->IsString()) {
-      data = v8::Local<v8::String>::Cast(args[4]);
+
+    //Check and set options array (ignores all non-array values)
+    if(argsLength >= 3) {
+        if (args[3]->IsArray()) {
+            options = Handle<Array>::Cast(args[3]);
+        }
+        else{
+            options = Array::New(0);
+        }
+    } 
+   
+    if(argsLength == 6) {
+        if (args[4]->IsString()) {
+            data = Local<String>::Cast(args[4]);
+        }
     }
-  }
 
-  int mask = 0;
-  for (unsigned int i = 0; i < options->Length(); i++) {
-    v8::Local<v8::String> opt = v8::Local<v8::String>::Cast(options->Get(i));
-    if(opt == v8::String::New("bind")) {
-      mask |= MS_BIND;
-    } else if(opt == v8::String::New("readonly")) {
-      mask |= MS_RDONLY;
-    } else if(opt == v8::String::New("remount")) {
-      mask |= MS_REMOUNT;
+    //Set callback, if provided as last argument
+    if(args[argsLength - 1]->IsFunction()){
+        cb = Local<Function>::Cast(args[argsLength - 1]);
     }
-  }
 
-  v8::String::Utf8Value device(args[0]->ToString());
-  v8::String::Utf8Value path(args[1]->ToString());
-  v8::String::Utf8Value type(args[2]->ToString());
+    int mask = 0;
+    for (unsigned int i = 0; i < options->Length(); i++) {
+        Local<String> opt = Local<String>::Cast(options->Get(i));
+        if(opt == String::New("bind")) {
+            mask |= MS_BIND;
+        } else if(opt == String::New("readonly")) {
+            mask |= MS_RDONLY;
+        } else if(opt == String::New("remount")) {
+            mask |= MS_REMOUNT;
+        } else if(opt == String::New("noexec")){
+            mask |= MS_NOEXEC;
+        }
+    }
 
-  int mountNum = mount(*device, *path, *type, mask, *data);
-  
-  bool mountAction = (mountNum == 0) ? true : false;
+    String::Utf8Value device(args[0]->ToString());
+    String::Utf8Value path(args[1]->ToString());
+    String::Utf8Value type(args[2]->ToString());
 
-  v8::Local<v8::Value> mounted = v8::Local<v8::Value>::New(v8::Boolean::New(mountAction));
+    int mountNum = mount(*device, *path, *type, mask, *data);
 
-  if(callback) {
-    const unsigned argc = 1;
-    v8::Local<v8::Value> argv[argc] = {mounted};
-    cb->Call(v8::Context::GetCurrent()->Global(), argc, argv);
-  }
-  return mounted;
+    bool mountAction = (mountNum == 0) ? true : false;
+
+    Local<Value> mounted = Local<Value>::New(Boolean::New(mountAction));
+
+    //Execute callback, if provided 
+    if(cb->IsFunction()) {
+        const unsigned argc = 1;
+        Local<Value> argv[argc] = {mounted};
+        cb->Call(Context::GetCurrent()->Global(), argc, argv);
+    }
+
+    return scope.Close(mounted); 
 }
 
-v8::Handle<v8::Value> Unmount(const v8::Arguments &args) {
-  v8::HandleScope scope;
+Handle<Value> Umount(const Arguments &args) {
+    HandleScope scope;
 
-  if (args.Length() < 1) {
-    return v8::ThrowException(v8::String::New("`umount` needs at least 1 parameter"));
-  }
+    if (args.Length() < 1) {
+        return ThrowException(String::New("Missing target"));
+    }
 
-  bool callback = false;
-  v8::Local<v8::Function> cb;
-  
-  if(args.Length() == 2) {
-    callback = true;
-    cb = v8::Local<v8::Function>::Cast(args[1]);
-  }
+    bool callback = false;
+    Local<Function> cb;
 
-  v8::String::Utf8Value path(args[0]->ToString());
+    if(args.Length() == 2) {
+        callback = true;
+        cb = Local<Function>::Cast(args[1]);
+    }
 
-  bool mountAction = (umount(*path) == 0) ? true : false;
+    String::Utf8Value path(args[0]->ToString());
 
-  v8::Local<v8::Value> mounted = v8::Local<v8::Value>::New(v8::Boolean::New(mountAction));
+    bool mountAction = (umount(*path) == 0) ? true : false;
 
-  if(callback) {
-    const unsigned argc = 1;
-    v8::Local<v8::Value> argv[argc] = {mounted};
-    cb->Call(v8::Context::GetCurrent()->Global(), argc, argv);
-  }
-  return mounted;
+    Local<Value> mounted = Local<Value>::New(Boolean::New(mountAction));
+
+    if(callback) {
+        const unsigned argc = 1;
+        Local<Value> argv[argc] = {mounted};
+        cb->Call(Context::GetCurrent()->Global(), argc, argv);
+    }
+
+    return scope.Close(mounted);
 }
 
-void init (v8::Handle<v8::Object> exports, v8::Handle<v8::Object> module) {
-  exports->Set(v8::String::NewSymbol("mount"), v8::FunctionTemplate::New(Mount)->GetFunction());
-  exports->Set(v8::String::NewSymbol("unmount"), v8::FunctionTemplate::New(Unmount)->GetFunction());
+void init (Handle<Object> exports, Handle<Object> module) {
+    exports->Set(String::NewSymbol("mount"), FunctionTemplate::New(Mount)->GetFunction());
+    exports->Set(String::NewSymbol("umount"), FunctionTemplate::New(Umount)->GetFunction());
 }
+
 NODE_MODULE(mount, init)
-
